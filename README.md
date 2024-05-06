e501a7919b4903ebc4a61cca8886ab36f5f770a8
r57zone <r57zone@gmail.com>
2024-05-06 17:39:55 +0400

    Improvements: support for button profiles, DInput pedal support

--- README.md
+++ README.md
@@ -2,30 +2,27 @@
 [![RU](https://user-images.githubusercontent.com/9499881/27683795-5b0fbac6-5cd8-11e7-929c-057833e01fb1.png)](https://github.com/r57zone/LogitechWingmanArduinoPedals/blob/master/README.RU.md)
 ← Choose language | Выберите язык
 
-# Logitech WingMan с Arduino педалями
-Xbox emulator for Logitech WingMan wheel [with modified pedals](https://github.com/r57zone/XboxExternalPedals) changed to work via Arduino. If you have a regular steering wheel with pedals, then use [x360ce](https://www.x360ce.com/) to emulate an Xbox controller, if there are no pedals, then you can use this project, triggers can be emulated with the buttons on the steering wheel. Powered by [ViGEm](https://github.com/ViGEm) driver.
+# Logitech WingMan Xbox Gamepad
+Xbox gamepad emulator for Logitech WingMan steering wheel (will probably fit other steering wheels as well). Supports [external modified pedals](https://github.com/r57zone/XboxExternalPedals).
 
 # Working modes
-The first mode works if Arduino pedals are connected.
+There are 4 operating modes of the program:
+* Steering wheel + standard pedals;
+* Steering wheel + external pedals (DInput or Arduino)
+* Steering wheel without pedals, instead of them there are buttons on the steering wheel.
 
-The front 2 buttons on the steering wheel work like `B` and `A`, the top 2 buttons on the steering wheel work like `Back` and `Start`, and the back buttons work like `X` and `Y`.
-
-
-
-The second mode is enabled when Arduino pedals are not connected.
-
-The front 2 buttons of the steering wheel work as triggers for the Xbox gamepad and you can play without pedals. The top 2 buttons work as `Back` and `Start`, and the back buttons as `B` and `A`.
+Buttons can be changed in the profile file. You can switch them in the program. You can also set the default mode and profile in the `Config.ini` configuration file.
 
 ## Setup
 1. Install [ViGEmBus](https://github.com/ViGEm/ViGEmBus/releases).
 2. Install Microsoft Visual C++ Redistributable 2017 or later.
-3. Connect the steering wheel and [external Arduino pedals](https://github.com/r57zone/XboxExternalPedals) (optional).
-4. Enter the pedal's COM port number into the `Config.ini` configuration file.
-5. Run the program and check the work in the XInputTest program.
-6. If necessary, calibrate the steering wheel by entering the data from the program into the configuration file. Data can be displayed by pressing `ALT + F9` and by pressing `ALT + F8`.
+3. Connect the steering wheel and standard pedals or [external modified pedals](https://github.com/r57zone/XboxExternalPedals).
+4. If you use non-standard pedals, you can enable it in the configuration file by entering the number of the COM port of the pedals or by turning on the search for DInput pedals.
+5. Run the program and check the work in the `XInputTest` program.
+6. If necessary, calibrate the steering wheel by entering the data from the program into the configuration file. Data can be displayed by pressing `ALT + F9`.
 
 ## Download
->Version for Windows 10.
+>Version for Windows 10, 11.
 
 **[Download](https://github.com/r57zone/LogitechWingmanArduinoPedals/releases)**
 
--- Source/LogitechWingMan.cpp
+++ Source/LogitechWingMan.cpp
@@ -7,10 +7,19 @@
 #include "IniReader\IniReader.h"
 #include <thread>
 #include <mmsystem.h>
+#include <atlstr.h>
 
 #pragma comment(lib, "winmm.lib")
 
-bool ExternalPedalsConnected = false;
+std::vector <std::string> GamepadProfiles;
+int ProfileIndex = 0;
+
+bool ExternalPedalsDInputConnected = false;
+JOYINFOEX ExternalPedalsJoyInfo;
+JOYCAPS ExternalPedalsJoyCaps;
+int ExternalPedalsJoyIndex = JOYSTICKID1;
+
+bool ExternalPedalsArduinoConnected = false;
 HANDLE hSerial;
 std::thread *pArduinoReadThread = NULL;
 float PedalsValues[2];
@@ -35,7 +44,7 @@ void ArduinoRead()
 {
 	DWORD bytesRead;
 
-	while (ExternalPedalsConnected) {
+	while (ExternalPedalsArduinoConnected) {
 		ReadFile(hSerial, &PedalsValues, sizeof(PedalsValues), &bytesRead, 0);
 
 		if (PedalsValues[0] > 1.0 || PedalsValues[0] < 0 || PedalsValues[1] > 1.0 || PedalsValues[1] < 0)
@@ -73,16 +82,162 @@ SHORT ToLeftStick(double Value)
 	return trunc(Value);
 }
 
+int clamp(int value, int min_value, int max_value) {
+	if (value < min_value)
+		return min_value;
+	else if (value > max_value)
+		return max_value;
+	else
+		return value;
+}
+
+bool IsKeyPressed(int KeyCode) {
+	return (GetAsyncKeyState(KeyCode) & 0x8000) != 0;
+}
+
+int GamepadKeyNameToKeyCode(std::string KeyName) {
+	std::transform(KeyName.begin(), KeyName.end(), KeyName.begin(), ::toupper);
+
+	if (KeyName == "NONE") return 0;
+
+	else if (KeyName == "DPAD-UP") return XINPUT_GAMEPAD_DPAD_UP;
+	else if (KeyName == "DPAD-DOWN") return XINPUT_GAMEPAD_DPAD_DOWN;
+	else if (KeyName == "DPAD-LEFT") return XINPUT_GAMEPAD_DPAD_LEFT;
+	else if (KeyName == "DPAD-RIGHT") return XINPUT_GAMEPAD_DPAD_RIGHT;
+	else if (KeyName == "BACK") return XINPUT_GAMEPAD_BACK;
+	else if (KeyName == "START") return XINPUT_GAMEPAD_START;
+	else if (KeyName == "LEFT-STICK") return XINPUT_GAMEPAD_LEFT_THUMB;
+	else if (KeyName == "RIGHT-STICK") return XINPUT_GAMEPAD_RIGHT_THUMB;
+	else if (KeyName == "LEFT-SHOULDER") return XINPUT_GAMEPAD_LEFT_SHOULDER;
+	else if (KeyName == "RIGHT-SHOULDER") return XINPUT_GAMEPAD_RIGHT_SHOULDER;
+	else if (KeyName == "A") return XINPUT_GAMEPAD_A;
+	else if (KeyName == "B") return XINPUT_GAMEPAD_B;
+	else if (KeyName == "X") return XINPUT_GAMEPAD_X;
+	else if (KeyName == "Y") return XINPUT_GAMEPAD_Y;
+	else if (KeyName == "BUTTON1") return JOY_BUTTON1;
+	else if (KeyName == "BUTTON2") return JOY_BUTTON2;
+	else if (KeyName == "BUTTON3") return JOY_BUTTON3;
+	else if (KeyName == "BUTTON4") return JOY_BUTTON4;
+	else if (KeyName == "BUTTON5") return JOY_BUTTON5;
+	else if (KeyName == "BUTTON6") return JOY_BUTTON6;
+
+	else return 0;
+}
+
+struct _ProfileButtons {
+	int Button1 = 0;
+	int Button2 = 0;
+	int Button3 = 0;
+	int Button4 = 0;
+	int Button5 = 0;
+	int Button6 = 0;
+	int LeftPedal = 0;
+	int RightPedal = 0;
+}; _ProfileButtons ProfileButtons;
+
+void LoadProfile(std::string ProfileFile) {
+	CIniReader IniFile("Profiles\\" + ProfileFile);
+	ProfileButtons.Button1 = GamepadKeyNameToKeyCode(IniFile.ReadString("GAMEPAD", "BUTTON1", "NONE"));
+	ProfileButtons.Button2 = GamepadKeyNameToKeyCode(IniFile.ReadString("GAMEPAD", "BUTTON2", "NONE"));
+	ProfileButtons.Button3 = GamepadKeyNameToKeyCode(IniFile.ReadString("GAMEPAD", "BUTTON3", "NONE"));
+	ProfileButtons.Button4 = GamepadKeyNameToKeyCode(IniFile.ReadString("GAMEPAD", "BUTTON4", "NONE"));
+	ProfileButtons.Button5 = GamepadKeyNameToKeyCode(IniFile.ReadString("GAMEPAD", "BUTTON5", "NONE"));
+	ProfileButtons.Button6 = GamepadKeyNameToKeyCode(IniFile.ReadString("GAMEPAD", "BUTTON6", "NONE"));
+	ProfileButtons.LeftPedal = GamepadKeyNameToKeyCode(IniFile.ReadString("GAMEPAD", "LEFT-PEDAL", "NONE"));
+	ProfileButtons.RightPedal = GamepadKeyNameToKeyCode(IniFile.ReadString("GAMEPAD", "RIGHT-PEDAL", "NONE"));
+}
+
+void ExternalPedalsDInputSearch() {
+	ExternalPedalsDInputConnected = false;
+	if (joyGetPosEx(JOYSTICKID1, &ExternalPedalsJoyInfo) == JOYERR_NOERROR &&
+		joyGetDevCaps(JOYSTICKID1, &ExternalPedalsJoyCaps, sizeof(ExternalPedalsJoyCaps)) == JOYERR_NOERROR &&
+		ExternalPedalsJoyCaps.wNumButtons == 16) { // DualSense - 15, DigiJoy - 16
+		ExternalPedalsJoyIndex = JOYSTICKID1;
+		ExternalPedalsDInputConnected = true;
+
+		joyGetDevCaps(JOYSTICKID1, &ExternalPedalsJoyCaps, sizeof(ExternalPedalsJoyCaps));
+	} 
+	else if (joyGetPosEx(JOYSTICKID2, &ExternalPedalsJoyInfo) == JOYERR_NOERROR &&
+		joyGetDevCaps(JOYSTICKID2, &ExternalPedalsJoyCaps, sizeof(ExternalPedalsJoyCaps)) == JOYERR_NOERROR &&
+		ExternalPedalsJoyCaps.wNumButtons == 16) {
+		ExternalPedalsJoyIndex = JOYSTICKID2;
+		ExternalPedalsDInputConnected = true;
+
+		joyGetDevCaps(1, &ExternalPedalsJoyCaps, sizeof(ExternalPedalsJoyCaps));
+	}
+	else if (joyGetPosEx(2, &ExternalPedalsJoyInfo) == JOYERR_NOERROR && // Оказывается может быть джойстик 3
+		joyGetDevCaps(2, &ExternalPedalsJoyCaps, sizeof(ExternalPedalsJoyCaps)) == JOYERR_NOERROR &&
+		ExternalPedalsJoyCaps.wNumButtons == 16) {
+		ExternalPedalsJoyIndex = 2;
+		ExternalPedalsDInputConnected = true;
+
+		joyGetDevCaps(2, &ExternalPedalsJoyCaps, sizeof(ExternalPedalsJoyCaps));
+	}
+}
+
+struct _AppStatus {
+	bool WheelConneted = false;
+	int WheelMode = 0;
+	//bool ExternalPedalsDInputConnected = false;
+	//bool ExternalPedalsArduinoConnected = false;
+	bool ExternalPedalsDInputSearch = false;
+
+	bool Calibration = false;
+
+}; _AppStatus AppStatus;
+
+void MainTextUpdate() {
+	system("cls");
+	printf("\n");
+	if (AppStatus.WheelConneted)
+		printf(" Wheel connected.\n");
+	else
+		printf(" Wheel not connected.\n");
+
+	if (AppStatus.WheelMode == 0)
+		printf(" Mode: wheel + default pedals.\n");
+	else if (AppStatus.WheelMode == 1)
+		printf(" Mode: wheel + external pedals.\n");
+	else
+		printf(" Mode: only wheel.\n");
+	printf(" Change mode with \"ALT + Left | Right\"\n");
+
+	if (ExternalPedalsDInputConnected)
+		printf(" External pedals DInput connected.\n");
+	if (ExternalPedalsArduinoConnected)
+		printf(" External pedals Arduino connected.\n");
+
+	printf_s(" Gamepad profile: %s.\n Change profiles with \"ALT + Up | Down\"\n", GamepadProfiles[ProfileIndex].c_str());
+
+	printf(" Press ALT + F9 for get calibration data.\n");
+}
+
 int main(int argc, char **argv)
 {
-	SetConsoleTitle("Logitech WingMan 1.0");
+	SetConsoleTitle("Logitech WingMan 1.1");
 	// Config parameters
 	CIniReader IniFile("Config.ini");
 
-	int SleepTimeOut = IniFile.ReadInteger("Gamepad", "SleepTimeOut", 1);
+	AppStatus.WheelMode = IniFile.ReadInteger("Wheel", "Mode", 0);
+	int ButtonCount = IniFile.ReadInteger("Wheel", "ButtonCount", 6);
+	int PedalsDeadZone = IniFile.ReadInteger("Wheel", "PedalsDeadZone", 0) * 2.55;
+	float DeadZone = IniFile.ReadInteger("Wheel", "DeadZone", 0);
+	int CalibMin = IniFile.ReadInteger("Wheel", "Min", 2816);
+	int CalibMax = IniFile.ReadInteger("Wheel", "Max", 41090);
+	int CalibCenter = IniFile.ReadInteger("Wheel", "Center", 32767);
+	int CalibCenterExternalPedals = IniFile.ReadInteger("Wheel", "CenterExternalPedals", 41090);
+	int WheelPercentCenterOffset = IniFile.ReadInteger("Gamepad", "WheelPercentCenterOffset", 0);
+	float Sensitivity = IniFile.ReadInteger("Gamepad", "Sensitivity", 100) / 100.0f;
 
-	int ExternalPedalsCOMPort = IniFile.ReadInteger("ExternalPedals", "COMPort", 3);
-	if (ExternalPedalsCOMPort != 0) {
+	int SleepTimeOut = IniFile.ReadInteger("Gamepad", "SleepTimeOut", 1);
+	AppStatus.ExternalPedalsDInputSearch = IniFile.ReadBoolean("ExternalPedals", "DInput", false);
+	ExternalPedalsJoyInfo.dwFlags = JOY_RETURNALL;
+	ExternalPedalsJoyInfo.dwSize = sizeof(ExternalPedalsJoyInfo);
+	int ExternalPedalsCOMPort = IniFile.ReadInteger("ExternalPedals", "COMPort", 0);
+
+	if (AppStatus.ExternalPedalsDInputSearch) // Dinput in priority
+		ExternalPedalsDInputSearch();
+	else if (ExternalPedalsCOMPort != 0) {
 		char sPortName[32];
 		sprintf_s(sPortName, "\\\\.\\COM%d", ExternalPedalsCOMPort);
 
@@ -102,7 +257,7 @@ int main(int argc, char **argv)
 
 				if (SetCommState(hSerial, &dcbSerialParams))
 				{
-					ExternalPedalsConnected = true;
+					ExternalPedalsArduinoConnected = true;
 					PurgeComm(hSerial, PURGE_TXCLEAR | PURGE_RXCLEAR);
 					pArduinoReadThread = new std::thread(ArduinoRead);
 				}
@@ -110,7 +265,28 @@ int main(int argc, char **argv)
 		}
 	}
 
+	// Search profiles
+	WIN32_FIND_DATA ffd;
+	HANDLE hFind = INVALID_HANDLE_VALUE;
+	hFind = FindFirstFile("Profiles\\*.ini", &ffd);
+	GamepadProfiles.push_back("Default.ini");
+	std::string DefaultProfile = IniFile.ReadString("Gamepad", "DefaultProfile", "Default.ini");
+	int ProfileCount = 0;
+	if (hFind != INVALID_HANDLE_VALUE) {
+		do {
+			if (strcmp(ffd.cFileName, "Default.ini")) { // Already added to the top of the list
+				GamepadProfiles.push_back(ffd.cFileName);
+				ProfileCount++;
+				if (strcmp(ffd.cFileName, DefaultProfile.c_str()) == 0)
+					ProfileIndex = ProfileCount;
+			}
+		} while (FindNextFile(hFind, &ffd) != 0);
+		FindClose(hFind);
+	}
+	LoadProfile(GamepadProfiles[ProfileIndex]);
+
 	int SkipPollCount = 0;
+	int TickCount = 0;
 
 	const auto client = vigem_alloc();
 	auto ret = vigem_connect(client);
@@ -124,105 +300,165 @@ int main(int argc, char **argv)
 	JOYINFOEX joyInfo;
 	joyInfo.dwFlags = JOY_RETURNALL;
 	joyInfo.dwSize = sizeof(joyInfo);
+	JOYCAPS JoyCaps;
 
 	int JoyIndex = JOYSTICKID1;
 
-	if (joyGetPosEx(JOYSTICKID1, &joyInfo) == JOYERR_NOERROR)
+	if (joyGetPosEx(JOYSTICKID1, &joyInfo) == JOYERR_NOERROR &&
+		joyGetDevCaps(JOYSTICKID1, &JoyCaps, sizeof(JoyCaps)) == JOYERR_NOERROR && JoyCaps.wNumButtons == ButtonCount) {
 		JoyIndex = JOYSTICKID1;
-	else if (joyGetPosEx(JOYSTICKID2, &joyInfo) == JOYERR_NOERROR)
+		AppStatus.WheelConneted = true;
+	}
+	else if (joyGetPosEx(JOYSTICKID2, &joyInfo) == JOYERR_NOERROR &&
+		joyGetDevCaps(JOYSTICKID2, &JoyCaps, sizeof(JoyCaps)) == JOYERR_NOERROR && JoyCaps.wNumButtons == ButtonCount) {
 		JoyIndex = JOYSTICKID2;
-
-	printf("\n");
-	if (joyGetPosEx(JoyIndex, &joyInfo) == JOYERR_NOERROR)
-		printf(" Wheel connected.\n");
-	else
-		printf(" Wheel not connected.\n");
+		AppStatus.WheelConneted = true;
+	}
 	
-	int CalibMin = IniFile.ReadInteger("Gamepad", "WheelMin", 2816);
-	int CalibMax = IniFile.ReadInteger("Gamepad", "WheelMax", 41090);
-	int WheelPercentCenterOffset = IniFile.ReadInteger("Gamepad", "WheelPercentCenterOffset", 0);
-	float Sensitivity = IniFile.ReadInteger("Gamepad", "Sensitivity", 100) / 100.0f;
-
-	
-	if (ExternalPedalsConnected)
-		printf(" External pedals connected\n");
-
-	printf(" Press ALT + F8 for show wheel percent.\n");
-	printf(" Press ALT + F9 for get calibration data.\n");
-
-	bool Calibration = false;
-	bool ShowPercentWheel = false;
+	MainTextUpdate();
 
 	while (! ( GetAsyncKeyState(VK_LMENU) & 0x8000 && GetAsyncKeyState(VK_ESCAPE) & 0x8000 ) )
 	{
 		XUSB_REPORT_INIT(&report);
+		
+		AppStatus.WheelConneted = joyGetPosEx(JoyIndex, &joyInfo) == JOYERR_NOERROR;
 
-		joyGetPosEx(JoyIndex, &joyInfo);
+		if (AppStatus.WheelConneted == false) {
+			ret = vigem_target_x360_update(client, x360, report);
+			Sleep(SleepTimeOut);
+		}
+		
+		if (SkipPollCount == 0) {
+			// Переключение профилей
+			if ((IsKeyPressed(VK_MENU) && (IsKeyPressed(VK_UP) || IsKeyPressed(VK_DOWN))) && GetConsoleWindow() == GetForegroundWindow())
+				{
+					SkipPollCount = 15;
+					if (IsKeyPressed(VK_UP)) if (ProfileIndex > 0) ProfileIndex--; else ProfileIndex = GamepadProfiles.size() - 1;
+					if (IsKeyPressed(VK_DOWN)) if (ProfileIndex < GamepadProfiles.size() - 1) ProfileIndex++; else ProfileIndex = 0;
+					LoadProfile(GamepadProfiles[ProfileIndex]);
+					MainTextUpdate();
+				}
 
-		// Calibration
-		if (((GetAsyncKeyState(VK_MENU) & 0x8000) != 0) && (GetAsyncKeyState(VK_F9) & 0x8000) != 0  && SkipPollCount == 0)
-		{
-			SkipPollCount = 15;
-			Calibration = !Calibration;
-			if (Calibration) {
-				CalibMax = 0;
-				CalibMin = 65535;
+			// Смена режима
+			if ((IsKeyPressed(VK_MENU) && (IsKeyPressed(VK_LEFT) || IsKeyPressed(VK_RIGHT))))
+			{
+				SkipPollCount = 15;
+				if (IsKeyPressed(VK_LEFT)) if (AppStatus.WheelMode > 0) AppStatus.WheelMode--; else AppStatus.WheelMode = 2;
+				if (IsKeyPressed(VK_RIGHT)) if (AppStatus.WheelMode < 2) AppStatus.WheelMode++; else AppStatus.WheelMode = 0;
+				MainTextUpdate();
 			}
-			system("cls");
-			printf(" Turn the steering wheel to left and right, then the save the values to config and restart app.\n");
-			Sleep(4000);
+
+			// Калибровка
+			if (((GetAsyncKeyState(VK_MENU) & 0x8000) != 0) && (GetAsyncKeyState(VK_F9) & 0x8000) != 0)
+			{
+				SkipPollCount = 15;
+				AppStatus.Calibration = !AppStatus.Calibration;
+				if (AppStatus.Calibration) {
+					CalibMax = 0;
+					CalibMin = 65535;
+					system("cls");
+					printf("\n Turn the steering wheel to left and right, then the save the values to config and restart app.\n");
+					TickCount = 300;
+				} else
+					MainTextUpdate();
+			}
+
 		}
 
-		if (Calibration) {
+		if (AppStatus.Calibration && TickCount == 0) {
 			if (joyInfo.dwXpos < CalibMin)
 				CalibMin = joyInfo.dwXpos;
 			if (joyInfo.dwXpos > CalibMax)
 				CalibMax = joyInfo.dwXpos;
-			printf(" Current=%d Min=%d, Max=%d\n", joyInfo.dwXpos, CalibMin, CalibMax);
+			printf(" Center=%d Min=%d, Max=%d\n", joyInfo.dwXpos, CalibMin, CalibMax);
+		}	
+
+		report.wButtons |= joyInfo.dwButtons & JOY_BUTTON1 ? ProfileButtons.Button1 : 0; // Бампер слева за рулём
+		report.wButtons |= joyInfo.dwButtons & JOY_BUTTON2 ? ProfileButtons.Button2 : 0; // Бампер справа за рулём
+		report.wButtons |= joyInfo.dwButtons & JOY_BUTTON3 ? ProfileButtons.Button3 : 0; // Сверху слева руля
+		report.wButtons |= joyInfo.dwButtons & JOY_BUTTON4 ? ProfileButtons.Button4 : 0; // Сверху справа руля
+		report.wButtons |= joyInfo.dwButtons & JOY_BUTTON5 ? ProfileButtons.Button5 : 0; // Слева спереди на руле
+		report.wButtons |= joyInfo.dwButtons & JOY_BUTTON6 ? ProfileButtons.Button6 : 0; // Справа спереди на руле
+
+		SHORT AxisX = 0;
+		int AxisPercent = 0;
+
+		// Режим руль + стандартные педали
+		if (AppStatus.WheelMode == 0) {
+			//int WheelAxis = clamp(joyInfo.dwXpos - 32767, -32767, 32767);
+
+			//report.sThumbLX = WheelAxis;
+			if (joyInfo.dwXpos > CalibCenter) {
+				AxisPercent = (clamp(joyInfo.dwXpos - CalibCenter, 0, 65535)) * 100 / (65535 - CalibCenter);
+				if (AxisPercent <= DeadZone) AxisPercent = 0;
+				AxisX = clamp(AxisPercent * 327.67 * Sensitivity, 0, 32767);
+				//printf("%d\t%d\t%d\n", joyInfo.dwXpos, AxisX, AxisPercent);
+			} else {
+				AxisPercent = 100 - (joyInfo.dwXpos * 100) / (65535 - CalibCenter);
+				if (AxisPercent <= DeadZone) AxisPercent = 0;
+				AxisX = clamp(AxisPercent * -327.67, -32767, 0);
+				//printf("%d\t%d\t%d\n", joyInfo.dwXpos, AxisX, AxisPercent);
+			}
+
+			//printf("%d %d\n", joyInfo.dwXpos, WheelAxis);
+
+			int LeftPedal = clamp((clamp(joyInfo.dwYpos, 32767, 65535) - 32767) / 128, 0, 255);
+			int RightPedal = clamp((32767 - clamp(joyInfo.dwYpos, 0, 32767)) / 128, 0, 255);
+
+			// Мертвая зона
+			if (LeftPedal < PedalsDeadZone) LeftPedal = 0;
+			if (RightPedal < PedalsDeadZone) RightPedal = 0;
+
+			report.bLeftTrigger = LeftPedal;
+			report.bRightTrigger = RightPedal;
 		}
 
-		else
-			report.sThumbLX = ToLeftStick(((joyInfo.dwXpos * 100 / (CalibMax - CalibMin) + WheelPercentCenterOffset) * 655.35 - 32767) * Sensitivity);
+		// Режим руль + внешние педали
+		else if (AppStatus.WheelMode == 1) {
+			if (ExternalPedalsDInputConnected) {
+				if (joyGetPosEx(ExternalPedalsJoyIndex, &ExternalPedalsJoyInfo) == JOYERR_NOERROR) {
+					report.bLeftTrigger = ExternalPedalsJoyInfo.dwVpos / 256;
+					report.bRightTrigger = ExternalPedalsJoyInfo.dwUpos / 256;
+				}
+			} else if (ExternalPedalsArduinoConnected) {
+				report.bLeftTrigger = PedalsValues[0] * 255;
+				report.bRightTrigger = PedalsValues[1] * 255;
+			}
+		}
 
-		if (((GetAsyncKeyState(VK_MENU) & 0x8000) != 0) && (GetAsyncKeyState(VK_F8) & 0x8000) != 0 && SkipPollCount == 0) 
+		// Режим руль без каких-либо педалей
+		else if (AppStatus.WheelMode == 2) {
+			report.bLeftTrigger = joyInfo.dwButtons & ProfileButtons.LeftPedal ? 255 : 0;
+			report.bRightTrigger = joyInfo.dwButtons & ProfileButtons.RightPedal ? 255 : 0;
+		}
+
+		//if (AppStatus.Calibration == false && AppStatus.ShowPercentWheel == false)
+		if (AppStatus.WheelMode == 1 || AppStatus.WheelMode == 2)
 		{
-			ShowPercentWheel = !ShowPercentWheel;
-			SkipPollCount = 15;
-			printf(" The percentage must be between 0 and 100, if it is not, then change the offset parameter in the config and restart app.\n");
-			Sleep(4000);
+			if (joyInfo.dwXpos > CalibCenterExternalPedals) {
+				AxisPercent = (clamp(joyInfo.dwXpos - CalibCenterExternalPedals, 0, 65535)) * 100 / (CalibMax - CalibCenterExternalPedals);
+				if (AxisPercent <= DeadZone) AxisPercent = 0;
+				AxisX = clamp(AxisPercent * 327.67 * Sensitivity, 0, 32767);
+				//printf("%d\t%d\t%d\n", joyInfo.dwXpos, AxisX, AxisPercent);
+			} else {
+				AxisPercent = 100 - (clamp(joyInfo.dwXpos - CalibMin, 0, 65535) * 100) / (CalibCenterExternalPedals - CalibMin);
+				if (AxisPercent <= DeadZone) AxisPercent = 0;
+				AxisX = clamp(AxisPercent * -327.67, -32767, 0);
+				//printf("%d\t%d\t%d\n", joyInfo.dwXpos, AxisX, AxisPercent);
+			}
 		}
-		if (ShowPercentWheel)
-			printf(" Wheel percent=%d\n", joyInfo.dwXpos * 100 / (CalibMax - CalibMin) + WheelPercentCenterOffset);
-
-		if (ExternalPedalsConnected) {
-			report.bLeftTrigger = PedalsValues[0] * 255;
-			report.bRightTrigger = PedalsValues[1] * 255;
-		}
-
-		report.wButtons |= joyInfo.dwButtons & JOY_BUTTON3 ? XINPUT_GAMEPAD_BACK : 0; // Сверху слева руля
-		report.wButtons |= joyInfo.dwButtons & JOY_BUTTON4 ? XINPUT_GAMEPAD_START : 0; // Сверху справа руля
-
-		if (ExternalPedalsConnected) {
-			report.wButtons |= joyInfo.dwButtons & JOY_BUTTON5 ? XINPUT_GAMEPAD_B : 0; // Слева спереди на руле
-			report.wButtons |= joyInfo.dwButtons & JOY_BUTTON6 ? XINPUT_GAMEPAD_A : 0; // Справа спереди на руле
-			report.wButtons |= joyInfo.dwButtons & JOY_BUTTON1 ? XINPUT_GAMEPAD_X : 0; // Бампер слева за рулём
-			report.wButtons |= joyInfo.dwButtons & JOY_BUTTON2 ? XINPUT_GAMEPAD_Y : 0; // Бампер справа за рулём
-		} else {
-			report.bLeftTrigger = joyInfo.dwButtons & JOY_BUTTON5 ? 255 : 0;
-			report.bRightTrigger = joyInfo.dwButtons & JOY_BUTTON6 ? 255 : 0;
-			report.wButtons |= joyInfo.dwButtons & JOY_BUTTON1 ? XINPUT_GAMEPAD_B : 0; // Бампер слева за рулём
-			report.wButtons |= joyInfo.dwButtons & JOY_BUTTON2 ? XINPUT_GAMEPAD_A : 0; // Бампер справа за рулём
-		}
+		report.sThumbLX = AxisX;
 
 		if (SkipPollCount > 0) SkipPollCount--;
+		if (TickCount > 0) TickCount--;
 		
 		ret = vigem_target_x360_update(client, x360, report);
 
 		Sleep(SleepTimeOut);
 	}
 
-	if (ExternalPedalsConnected) {
-		ExternalPedalsConnected = false;
+	if (ExternalPedalsArduinoConnected) {
+		ExternalPedalsArduinoConnected = false;
 		pArduinoReadThread->join();
 		delete pArduinoReadThread;
 		pArduinoReadThread = nullptr;
